7c3beeaca6038b60171f8b6dae0512435b5d0b58
Jacob Jerry Arackal <jacobjerryarackal123@gmail.com>
2024-07-10 16:10:11 +0530

    Files Added

---
+++ script.js
@@ -0,0 +1,42 @@
+// FAQ
+var acc = document.getElementsByClassName("accordion");
+      var i;
+
+      for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+          this.classList.toggle("active");
+          this.parentElement.classList.toggle("active");
+
+          var pannel = this.nextElementSibling;
+
+          if (pannel.style.display === "block") {
+            pannel.style.display = "none";
+          } else {
+            pannel.style.display = "block";
+          }
+        });
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.getElementById('hamburger');
+  const navButton = document.querySelector('.nav-button ul');
+
+  hamburger.addEventListener('click', function () {
+    navButton.classList.toggle('show');
+  });
+});
+
+function openTab(evt, details) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+  tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(details).style.display = "block";
+  evt.currentTarget.className += " active";
+}
